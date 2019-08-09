e4f48bc9865d838353f01c6a1fad108ae9e04005
Rohit Vighne <rohit.vighne@gmail.com>
2019-08-09 00:20:59 -0700

    Sass rendering on build (janky)

--- .eleventy.js
+++ .eleventy.js
@@ -1,3 +1,11 @@
-export default ec => {
+const fs = require('fs')
+const sass = require('node-sass')
+
+module.exports = ec => {
+	fs.writeFileSync('style/main.css', sass.renderSync({
+		file: 'style/main.scss'
+	}))
+
+	ec.setTemplateFormats(['html', 'md', 'css'])
 	ec.addPassthroughCopy('img')
 }
