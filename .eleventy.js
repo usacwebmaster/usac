b3fde70c5695c9c2d805fe294dd18b5781373dc6
Rohit Vighne <rohit.vighne@gmail.com>
2019-08-11 00:44:39 -0700

    Dynamic header nav

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,7 @@
 module.exports = ec => {
 	ec.addPassthroughCopy('img')
 	ec.addFilter('mainsection', url => url.match(/^\/([^/]*)/)[1] || 'home')
+	ec.addCollection('toplevel', c => c.getFilteredByGlob('*/index.*'))
 
 	return {
 		dataTemplateEngine: false,
