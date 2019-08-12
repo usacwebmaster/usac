a38b3e37f2031c7d1a543a2df24e229d43ad5709
Rohit Vighne <rohit.vighne@gmail.com>
2019-08-12 02:14:03 -0700

    Add officers page

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,14 @@
 module.exports = ec => {
-	ec.addPassthroughCopy('img')
+	const getToplevel = c => c.getFilteredByGlob('*/index.*')
+	const getSecondlevel = c => c.getFilteredByGlob('*/*/index.*')
+
 	ec.addFilter('mainsection', url => url.match(/^\/([^/]*)/)[1] || 'home')
-	ec.addCollection('toplevel', c => c.getFilteredByGlob('*/index.*'))
+	ec.addCollection('toplevel', getToplevel)
+	ec.addCollection('secondlevel', c => [...getToplevel(c), ...getSecondlevel(c)])
 
 	return {
+		templateFormats: ['html', 'md', 'jpg', 'svg'],
+
 		dataTemplateEngine: false,
 		markdownTemplateEngine: 'njk',
 		htmlTemplateEngine: 'njk'
