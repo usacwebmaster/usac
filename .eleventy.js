2cd2cafff2e69fa1e64e785c5faf5616810ae072
Rohit Vighne <rohit.vighne@gmail.com>
2019-08-24 18:34:12 -0700

    Lookup mainsection page from templates

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,8 @@
 module.exports = ec => {
-	ec.addFilter('mainsection', url => url.match(/^\/([^/]*)/)[1] || 'home')
-	ec.addFilter('descendsFrom', (desc, anc) => desc.startsWith(anc))
+	ec.addFilter('mainsection', (all, url) => all.find(page => page.url === url.match(/^\/[^/]*\/?/)[0]))
 	ec.addFilter('children', (all, url) => all.filter(page => page.url.startsWith(url) && !page.url.slice(url.length, -1).includes('/') && page.url !== url))
+
+	ec.addFilter('descendsFrom', (desc, anc) => desc.startsWith(anc))
 
 	ec.setLibrary('md', require('markdown-it')('commonmark').use(require('markdown-it-anchor'), {
 		permalink: true,
