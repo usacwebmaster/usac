e7f72266c0b643793d9da282d8c90985340fa053
Rohit Vighne <rohit.vighne@gmail.com>
2019-08-26 21:06:29 -0700

    Deterministic sort order for pages with optional override

--- .eleventy.js
+++ .eleventy.js
@@ -4,7 +4,9 @@ module.exports = ec => {
 	ec.addFilter('mainSection', (all, url) => all.find(page => page.url === url.match(/^\/[^/]*\/?/)[0]))
 	ec.addFilter('sectionName', page => page.fileSlug || 'home')
 
-	ec.addFilter('children', (all, url) => all.filter(page => page.url.startsWith(url) && !page.url.slice(url.length, -1).includes('/') && page.url !== url))
+	ec.addFilter('children', (all, url) =>
+		all.filter(page => page.url.startsWith(url) && !page.url.slice(url.length, -1).includes('/') && page.url !== url)
+			.sort(({ data: a }, { data: b }) => (a.weight || 0) - (b.weight || 0) || (a.title > b.title ? 1 : a.title < b.title ? -1 : 0)))
 	ec.addFilter('descendsFrom', (desc, anc) => desc.startsWith(anc))
 
 	ec.setLibrary('md', require('markdown-it')('commonmark').use(require('markdown-it-anchor'), {
