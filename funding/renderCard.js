f80227a8ff3fda94783f40b0d0c35629026423b0
yifan.you.37 <harry47341@yahoo.com>
2019-11-25 18:25:07 -0800

    html handlebars fund card

---
+++ funding/renderCard.js
@@ -0,0 +1,40 @@
+// compile the template
+
+var source = document.getElementById('card-template').innerHTML;
+var template = Handlebars.compile(source);
+
+
+function compileFundCards(fundArray, deadlineDict, causeDict, cardTemplate) {
+    console.log(fundArray)
+
+    var compiled = "";
+    var tempCard = null;
+    
+    for (var i = 0; i < 1; i++) {
+        tempCard = JSON.parse(JSON.stringify(fundArray[i]))
+        tempCard.tags.deadline = deadlineDict[tempCard.tags.deadline];
+        tempCard.tags.cause = tempCard.tags.cause.map( n => causeDict[n]);
+        eligibility_group_str = ""
+        eligibility_individual_str = ""
+
+        // console.log(tempCard.eligibility)
+        if (tempCard.eligibility.group == true) {
+            eligibility_group_str = "Groups must be registered with SOLE"
+        }
+        if (tempCard.eligibility.individual == true) {
+            eligibility_individual_str = "individuals may also apply"
+        }
+        tempCard.eligibility.group = eligibility_group_str
+        tempCard.eligibility.individual = eligibility_individual_str
+        // console.log(tempCard)
+        compiled += cardTemplate(tempCard);
+
+    }
+    
+    return compiled;
+}
+
+var fundHTML = compileFundCards(fundData, deadline_dict, cause_dict, template)
+
+document.getElementById("cards-list").innerHTML = fundHTML;
+ // execute the compiled template and print the output to the console
