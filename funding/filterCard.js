ff433e88e45c689dfb9ccd8bae84ddc4fa989a66
yifan.you.37 <harry47341@yahoo.com>
2019-12-04 10:55:25 -0800

    funding app 2 columns

--- funding/filterCard.js
+++ funding/filterCard.js
@@ -10,22 +10,31 @@ function causeIsPresent(card_causes, cause)
 function filterCards()
 {
 	var checked = [];
-	var c = [document.getElementById("1"), document.getElementById("2"), document.getElementById("3"),document.getElementById("4"),document.getElementById("5"),document.getElementById("6")];
-	for(var i = 0; i < 6; i++)
+	var c = [];
+	for(var key in cause_dict)
+	{
+		c.push(document.getElementById("c"+key));
+	}
+	
+	for(var i = 0; i < c.length; i++)
 		if(c[i].checked==true)
 			checked.push(i+1);
     if (checked.length == 0) {
-        for(var i = 0; i < 6; i++) {
+        for(var i = 0; i < c.length; i++) {
             checked.push(i);
         }
     }
 	var deadline = [];
-	var d = [document.getElementById("d1"), document.getElementById("d2")];
-	for(var i = 0; i<2; i++)
+	var d = [];
+	for(var key in deadline_dict)
+	{
+		d.push(document.getElementById("d"+key));
+	}
+	for(var i = 0; i<d.length; i++)
 		if(d[i].checked==true)
 			deadline.push(i+1);
     if (deadline.length == 0) {
-        for(var i = 0; i < 2; i++) {
+        for(var i = 0; i < d.length; i++) {
             deadline.push(i)
         }
     }
@@ -35,13 +44,35 @@ function filterCards()
 			filteredIds.push(fundData[i]["id"]);
     }
 	if (filteredIds.length!=0)
-        document.getElementById("text1").innerHTML = filteredIds;
-        displayFundsById(filteredIds)
-	//document.getElementById("t1").innerHTML = "Hello";
-		
-  //for(var i = 0; i<cards.length; i++)
-  //{
-    //if(isMatch(cards[i], deadline, cause, eligibility))
-      //filtered_cards.push(cards[i];
-  //}
+	{
+    	document.getElementById("text1").innerHTML = checked;
+    	displayFundsById(filteredIds)
+    }
+}
+
+createCheckboxes();
+function createCheckboxes() {
+	for (var key in deadline_dict) {
+		var chk = document.createElement('input'); // CREATE CHECK BOX.
+		chk.setAttribute('type', 'checkbox'); // SPECIFY THE TYPE OF ELEMENT.
+		chk.setAttribute('id', "d" + key); // SET UNIQUE ID.
+		chk.setAttribute('value', deadline_dict[key]);
+
+		var lbl = document.createTextNode(deadline_dict[key] + ": ");
+		dline.appendChild(lbl)
+		dline.appendChild(chk);
+		document.getElementById("dline").innerHTML += ("<br/>");
+	}
+
+	for (var key in cause_dict) {
+		var chk = document.createElement('input'); // CREATE CHECK BOX.
+		chk.setAttribute('type', 'checkbox'); // SPECIFY THE TYPE OF ELEMENT.
+		chk.setAttribute('id', "c" + key); // SET UNIQUE ID.
+		chk.setAttribute('value', cause_dict[key]);
+
+		var lbl = document.createTextNode(cause_dict[key] + ": ");
+		cse.appendChild(lbl)
+		cse.appendChild(chk);
+		document.getElementById("cse").innerHTML += ("<br/>");
+	}
 }
