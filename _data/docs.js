78b5f649279a976a4e6481f1d9c0cbd6270bca85
Rohit Vighne <rohit.vighne@gmail.com>
2020-01-22 10:41:25 -0800

    Add agendas and update budget

--- _data/docs.js
+++ _data/docs.js
@@ -13,7 +13,7 @@ function getSchoolYear(date) {
 module.exports = async () => {
 	const index = {
 		_schoolYear: getSchoolYear(new Date),
-		_budgetDate: new Date("2019-01-13")
+		_budgetDate: new Date("2019-01-21")
 	}
 
 	const files = await fs.readdir(input)
