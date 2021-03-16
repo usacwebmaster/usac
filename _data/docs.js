4eb0f154f174c5d2a840753b8658b53bf1c58f30
Rohit Vighne <rohit.vighne@gmail.com>
2021-03-16 00:27:05 -0700

    Update TGMF fund docs, update budget, add minutes

--- _data/docs.js
+++ _data/docs.js
@@ -13,7 +13,7 @@ function getSchoolYear(date) {
 module.exports = async () => {
 	const index = {
 		_schoolYear: getSchoolYear(new Date),
-		_budgetDate: new Date('2021-03-09')
+		_budgetDate: new Date('2021-03-15')
 	}
 
 	const files = await fs.readdir(input)
