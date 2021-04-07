f9b61e69160178a04f7ff0893ecc61f9f0e824f1
Rohit Vighne <rohit.vighne@gmail.com>
2021-04-07 01:23:14 -0700

    Update budget

--- _data/docs.js
+++ _data/docs.js
@@ -13,7 +13,7 @@ function getSchoolYear(date) {
 module.exports = async () => {
 	const index = {
 		_schoolYear: getSchoolYear(new Date),
-		_budgetDate: new Date('2021-03-29')
+		_budgetDate: new Date('2021-04-05')
 	}
 
 	const files = await fs.readdir(input)
