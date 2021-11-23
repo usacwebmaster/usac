66ac282cc238306262184522482c5fa96a9c926f
Rohit Vighne <rohit.vighne@gmail.com>
2021-11-22 17:17:44 -0800

    Add agenda and budget

--- _data/docs.js
+++ _data/docs.js
@@ -14,7 +14,7 @@ function getSchoolYear(date) {
 module.exports = async () => {
 	const index = {
 		_schoolYear: getSchoolYear(new Date),
-		_budgetDate: new Date('2021-11-15')
+		_budgetDate: new Date('2021-11-22')
 	}
 
 	const files = await fs.readdir(input)
