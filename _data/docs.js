c1c6bf86a739023f7aef5579685d82731e5c4b88
fperez77 <95935141+fperez77@users.noreply.github.com>
2022-03-07 12:06:21 -0800

    Update docs.js

--- _data/docs.js
+++ _data/docs.js
@@ -14,7 +14,7 @@ function getSchoolYear(date) {
 module.exports = async () => {
 	const index = {
 		_schoolYear: getSchoolYear(new Date),
-		_budgetDate: new Date('2022-03-03')
+		_budgetDate: new Date('2022-03-07')
 	}
 
 	const files = await fs.readdir(input)
