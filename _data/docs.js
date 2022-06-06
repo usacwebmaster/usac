63565c42af220763cd279392c5e71b6e65106c34
the-aniketdas <aniketdas@ucla.edu>
2022-06-06 15:27:53 -0700

    budget

--- _data/docs.js
+++ _data/docs.js
@@ -14,7 +14,7 @@ function getSchoolYear(date) {
 module.exports = async () => {
 	const index = {
 		_schoolYear: getSchoolYear(new Date),
-		_budgetDate: new Date('2022-06-01')
+		_budgetDate: new Date('2022-06-06')
 	}
 
 	const files = await fs.readdir(input)
